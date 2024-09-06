f73ff309e384ab7499e7289a747178443d252157
Estudiante <luquiscardenas@gmail.com>
2024-09-06 14:30:15 -0300

    cambiando de compu

---
+++ backend-jwt/config/config.js
@@ -0,0 +1,7 @@
+export const GV = {};
+GV.SECRET_KEY = "aopz9-3fliw12-4ja94f-6l4jzg8";
+GV.PORT = 4000;
+GV.DB_NAME = "db_system";
+GV.DB_USER = "root";
+GV.DB_HOST = "localhost";
+GV.DB_PASSWORD = "";
--- backend-jwt/db/database.js
+++ backend-jwt/db/database.js
@@ -1,25 +1,22 @@
-export const database = 
-    {
-        user: [
-            {
-                id: 1,
-                username: 'admin',
-                password: 'admin'
-            },
-            {
-                id: 2,
-                username: 'user',
-                password: 'user'
-            },
-            {
-                id: 3,
-                username: 'guest',
-                password: 'guest'
-            },
-            {
-                id: 4,
-                username: 'guest2',
-                password: 'guest2'
-            }
-        ]
-    }
+import mysql from "mysql2";
+import { GV } from "../config/config.js";
+
+export const connection = mysql.createConnection({
+  host: GV.DB_HOST,
+  database: GV.DB_NAME,
+  user: GV.DB_USER,
+  password: GV.DB_PASSWORD,
+});
+
+export function createTable() {
+  try {
+    const sql = `CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(100) NOT NULL,
+        password VARCHAR(100) NOT NULL)`;
+    connection.query(sql);
+    console.log("Table 'users' created or already exists.");
+  } catch (error) {
+    console.error("Error creating 'users' table:", error);
+  }
+}
